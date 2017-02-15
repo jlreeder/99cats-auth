fd1a594a2baaf2791266f02c19672381ff11c28d
Jason Reeder <info@jasonreeder.com>
2017-02-15 14:37:09 -0800

    Add logic for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,28 @@
 class SessionsController < ApplicationController
   def new
+    render :new
   end
 
   def create
+    @user = User.find_by_credentials(
+      session_params[:user_name],
+      session_params[:password]
+    )
+
+    if @user
+      render text: "Successfully logged in!"
+    else
+      flash.now[:errors] << "Invalid credentials"
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:user_name, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 NinetyNineCatsDay1::Application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   resources :cats, except: :destroy
   resources :cat_rental_requests, only: [:create, :new] do
     post "approve", on: :member
     post "deny", on: :member
   end
 
-  resources :users, only: [:create, :new, :show]
+  resources :users, only: [:create, :new]
+  resource :session, only: [:create, :new, :destroy]
 
   root to: redirect("/cats")
 end
