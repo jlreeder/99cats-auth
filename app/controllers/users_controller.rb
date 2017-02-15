d4293c01c1a4411ace1179e8b8408c159ee6b933
Jason Reeder <info@jasonreeder.com>
2017-02-15 14:23:47 -0800

    Add render placeholder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_url(@user)
+      render text: "Successfully created user!"
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
